aef6936bf79602762064afedf9c702d04d6bffdc
johnnykoo84 <johnnykoo84@gmail.com>
2024-06-07 13:53:39 +0900

    Add plant html for single plant

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ from db import data
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('plants.html', plants=data)
-    return data
-    return "Hello World"
+
+@app.route('/<int:id>')
+def get_plant(id):
+    # find individual data with matched id
+    print('id', id)
+    max_len = len(data)
+    if id> max_len:
+        return 'Plant not found', 404
+    else:
+        plant = data[id]
+        if plant:
+            return render_template('plant.html', plant=plant)
+        else:
+            return 'Plant not found', 404
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
