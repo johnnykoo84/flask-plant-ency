10d0d5fc09c97eaf29d25fbf2b486507a38a667e
johnnykoo84 <johnnykoo84@gmail.com>
2024-06-07 14:05:16 +0900

    Add delete plant

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from db import data
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def get_all_plants():
     return render_template('plants.html', plants=data)
 
 @app.route('/<int:id>')
@@ -13,15 +13,26 @@ def get_plant(id):
     # find individual data with matched id
     print('id', id)
     max_len = len(data)
-    if id> max_len:
+    if id > max_len:
         return 'Plant not found', 404
     else:
-        plant = data[id]
+        plant = data[id-1]
         if plant:
             return render_template('plant.html', plant=plant)
         else:
             return 'Plant not found', 404
 
+@app.route("/delete/<int:id>", methods=['POST'])
+def delete_plant(id):
+    print('delete_plant', id)
+    max_len = len(data)
+    print('max_len', max_len)
+
+    if id > max_len:
+        return 'Plant not found', 404
+    else:
+        del data[id-1]
+        return redirect(url_for('get_all_plants'))
 
 
 if __name__ == '__main__':
