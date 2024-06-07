806ec7c92c4aabdd1b203a4fe8bfdc401fcc0f38
johnnykoo84 <johnnykoo84@gmail.com>
2024-06-07 13:32:29 +0900

    Add fake db

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from db import data
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return data
     return "Hello World"
 
 if __name__ == '__main__':
---
+++ db.py
@@ -0,0 +1,45 @@
+data = {
+    1: {
+        "plant_name": "깻잎",
+        "family_kor_nm": "깻잎과",
+        "family_nm": "Brassica",
+        "genus_kor_nm": "깻잎속",
+        "genus_nm": "Brassica",
+        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr8Uh-buvRfMZZXw4LYjN5OZthg6mmEIFsWA&s",
+        "desc": "깻잎은 맛있다"
+    },
+    2: {
+        "plant_name": "코코아잎",
+        "family_kor_nm": "코코아잎과",
+        "family_nm": "Coccaceae",
+        "genus_kor_nm": "코코아속",
+        "genus_nm": "Coccus",
+        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2GJiBJWcs7UlQQDc5ceHK1li4x22AboerzA&s",
+        "desc": "코코아 하하"
+        },
+    3: {
+        "plant_name": "소나무",
+        "family_kor_nm": "소나무과",
+        "family_nm": "Solanaceae",
+        "genus_kor_nm": "소나무속",
+        "genus_nm": "Solanum",
+        "img_url": "https://i.namu.wiki/i/HPXSQ7eVZsn3oSvNtBItitmeqVnuGq6W-yNzxjsiNAJBgwWM4lr-aIUvr1XEFMVdkEkaOfL4DsesDgaOaZVU-A.webp",
+        "desc": "소나무는 맛있다"
+        },
+    4: {
+        "plant_name": "무궁화",
+        "family_kor_nm": "무궁화과",
+        "family_nm": "Monocotyledon",
+        "genus_kor_nm": "무궁화속",
+        "genus_nm": "Monocotyledon",
+        "img_url": "https://i.namu.wiki/i/U3xs4ovgGfDKlDujDDa4amcUg9F4xjGKE5X3ZeNc6Jmg1NOxcy6HIJPLCDsNIHDlbgYAcrEui2-Z_H6VsJ921A.webp",
+        "desc": "무궁화는 맛있다"    },
+    5: {
+        "plant_name": "벚꽅",
+        "family_kor_nm": "벚꽃과",
+        "family_nm": "Monocotyledon",
+        "genus_kor_nm": "벚꽃속",
+        "genus_nm": "Monocotyledon",
+        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBCszt6vqLKVVlTYgd4QBy505uM-5gLsoCLA&s",
+        "desc": "벚꽃 놀이가자"    }
+}
