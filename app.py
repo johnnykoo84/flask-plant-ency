76e6e22f2680f980f5c0f923c4c35f3c3cfe9cb6
johnnykoo84 <johnnykoo84@gmail.com>
2024-06-07 13:41:55 +0900

    Update db format and add html teplates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from db import data
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('plants.html', plants=data)
     return data
     return "Hello World"
 
